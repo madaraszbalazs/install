e49d870165afbc7cd29b3e69157941f6ff94d6da
Balázs Madarász <balazs.madarasz@cooltix.hu>
2020-01-29 09:41:48 +0100

    first commit

---
+++ install-zsh.sh
@@ -0,0 +1,3 @@
+# wget -O - https://gitlab.com/balazs.madarasz/install/raw/master/install-zsh.sh | bash
+# wget -O - https://gitlab.com/api/v4/projects/install/repository/files/app%2Fmodels%2Fkey%2Erb/raw?ref=master | bash
+echo "helló $USER";
