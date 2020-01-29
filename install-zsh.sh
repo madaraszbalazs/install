2e8a7fd8e7c73be53b7aa74248ae2481c7ece62a
Balázs Madarász <balazs.madarasz@cooltix.hu>
2020-01-29 09:54:37 +0100

    add zsh

--- install-zsh.sh
+++ install-zsh.sh
@@ -1,3 +1,25 @@
-# wget -O - https://gitlab.com/balazs.madarasz/install/raw/master/install-zsh.sh | bash
-# wget -O - https://gitlab.com/api/v4/projects/install/repository/files/app%2Fmodels%2Fkey%2Erb/raw?ref=master | bash
-echo "helló $USER";
+# wget -O - https://raw.githubusercontent.com/madaraszbalazs/install/master/install-zsh.sh | bash
+
+# Install tilix and zsh
+sudo add-apt-repository ppa:webupd8team/terminix
+sudo apt-get update
+sudo apt-get install tilix zsh python3-pip
+sudo chsh -s /usr/bin/zsh
+
+# Install Ohmyzsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Install the Powerlevel9k Theme
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+
+mkdir /home/$USER/Downloads/nerd-fonts
+git clone https://github.com/ryanoasis/nerd-fonts.git /home/$USER/Downloads/nerd-fonts
+/home/$USER/Downloads/nerd-fonts/install.sh
+rm -rf /home/$USER/Downloads/nerd-fonts
+
+mkdir -p /home/$USER/Downloads/zshinstall
+git clone https://gitlab.com/balazs.madarasz/install.git /home/$USER/Downloads/zshinstall
+
+dconf load /com/gexperts/Tilix/ < /home/$USER/Downloads/zshinstall/tilix.dconf
+
+cp /home/$USER/Downloads/zshinstall/.zshrc /home/$USER/
